6eafb17150d8415f5475da2794f11bee240d7dd2
tfox1127 <tfox1127@gmail.com>
2025-01-14 07:00:27 -0600

    asdf

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ import os, random
 import datetime as dt
 from flask import Flask, render_template, request, jsonify, session
 
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker, scoped_session
 
-DATABASE_URL = os.environ['ATTACHED_DB_URL']
+try: 
+    DATABASE_URL = os.environ['ATTACHED_DB_URL']
+except: 
+    DATABASE_URL = os.environ['DATABASE_URL']
+
 DATABASE_URL = DATABASE_URL.replace("s://", "sql://", 1)
 
 app = Flask(__name__)
-# engine = create_engine(DATABASE_URL, isolation_level="AUTOCOMMIT")
-# db = scoped_session(sessionmaker(bind=engine))
+engine = create_engine(DATABASE_URL, isolation_level="AUTOCOMMIT")
+db = scoped_session(sessionmaker(bind=engine))
 
 # app.secret_key = 'your_secret_key'  # Replace with a secure secret key
 app.secret_key = os.environ['pizza']
@@ -32,12 +38,12 @@ def practice():
         original_word = request.form['original_word']
         original_word_original = original_word
 
-        # db.execute(text(
-        #     f"""
-        #         INSERT INTO ben_spells_logs (user_id, original_word_original, user_word, original_word, ts)
-        #         VALUES ({session['user_id']}, '{original_word_original}', '{user_word}', '{original_word}', '{dt.datetime.now()}');
-        #     """))
-        # db.commit()
+        db.execute(text(
+            f"""
+                INSERT INTO ben_spells_logs (user_id, original_word_original, user_word, original_word, ts)
+                VALUES ({session['user_id']}, '{original_word_original}', '{user_word}', '{original_word}', '{dt.datetime.now()}');
+            """))
+        db.commit()
 
         user_word = user_word.strip()
         user_word = user_word.replace('’', "'")
